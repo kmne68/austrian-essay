1647d48b82549e0b3bfe18bafa46fb9bc68343e6
kmne68 <kmne68@gmail.com>
2017-05-04 23:06:07 -0500

    Add random div

--- js/page_functions.js
+++ js/page_functions.js
@@ -23,9 +23,8 @@ function changeImage() {
            }).slice(0,4);
 $(divs).show();
 
-
+/*
 (function makeDiv(){
-    // vary size for fun
     var divsize = ((Math.random()*100) + 50).toFixed();
     var color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
     $newdiv = $('<div/>').css({
@@ -33,18 +32,17 @@ $(divs).show();
         'height':divsize+'px',
         'background-color': color
     });
-
-    // make position sensitive to size and document's width
+    
     var posx = (Math.random() * ($(document).width() - divsize)).toFixed();
     var posy = (Math.random() * ($(document).height() - divsize)).toFixed();
-
+    
     $newdiv.css({
         'position':'absolute',
         'left':posx+'px',
         'top':posy+'px',
         'display':'none'
-    }).appendTo( 'body' ).fadeIn(100).delay(1000).fadeOut(500, function(){
-      $(this).remove();
-      makeDiv(); 
+    }).appendTo( 'body' ).fadeIn(100).delay(300).fadeOut(200, function(){
+       $(this).remove();
+       makeDiv(); 
     }); 
-})();
+})(); */
